a00ae1bd7e7ce044b6eaeec737141e6be635cbe7
Sarah77777777 <sarah.adamon77@gmail.com>
2024-07-30 10:13:58 +0200

    javascript

---
+++ script.js
@@ -0,0 +1,25 @@
+/*let name = prompt("Quel est ton prénom ?");
+
+let welcomeTitle = document.getElementById("welcomeTitle");
+
+welcomeTitle.innerText = "Bienvenue " + name;*/
+let isOk = prompt (" Est-ce que JJK est le meilleur anime ? répondre par oui ou non");
+
+if((isOk == "biensur") || (isOk == "oui")|| (isOk == "toujours"))
+
+{
+//faire autre chose
+console.log ( "l'utilisateur n'est pas en tord")
+}
+else if ((isOk == "non") || (isOk == "jamais") || (isOk == "pas du tout"))
+
+{
+//faire autre chose
+console.log ( "l'utilisateur est en tord")	
+}
+else
+{
+	//faire une chose dans le cas ou l'utilisateur  fait ce qu'il veut
+	console.log ( "l'utilisateur est")
+
+}
